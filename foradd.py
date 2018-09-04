6af242e6d773ec4ba37950ccae52bef5cb827255
root <root@station217.example.com>
2018-09-04 18:06:09 +0530

    for iteration done

---
+++ foradd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env  python2
+
+import  sys,time
+#  taking all numbers to add 
+user_data=sys.argv[1:]
+sum=0
+for   i   in  user_data:
+	sum=sum+int(i)
+
+
+print  "addition of all given numbers :  ",sum
+
+
+
+
+
+
+
+
+
+
